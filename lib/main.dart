d760e1c65dd6e4732a8f813095ccc98397d8dd8d
chajinjoo <chajanee@gmail.com>
2020-03-02 23:35:28 +0900

    navigation demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screen1.dart';
+import 'screen0.dart';
+import 'screen2.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,8 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Screen1(),
-      //
+//      home: Screen0(),
+      //초기 경로인 initialRoute 와 중복이니 충돌난다. 이경우 home 속성은 지워주자.
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Screen0(),
+        '/first': (context) => Screen1(),
+        '/second': (context) => Screen2(),
+      },
     );
   }
 }
